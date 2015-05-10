fd64f7676989a12ccdac4025b1a13b63a88d3579
yjx <18602590523@163.com>
2015-05-10 18:40:23 +0800

    登陆

---
+++ app/src/main/java/com/yjx/cnblog/activity/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.yjx.cnblog.activity;/**
+ * Created by yjx on 15/5/10.
+ */
+
+import android.view.View;
+
+import com.yjx.cnblog.base.BaseActivity;
+
+/**
+ * User: YJX
+ * Date: 2015-05-10
+ * Time: 17:56
+ * 登陆页面
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
